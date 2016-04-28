267321a1539e2a9b8009dccf65fdd16aeb928925
Quentin <qperrot@stanford.edu>
2016-04-27 22:57:45 -0700

    fixed again
    no tab bar

---
+++ CatchUp/CatchUp/CatchUp/Section.swift
@@ -0,0 +1,17 @@
+//
+//  Section.swift
+//  CatchUp
+//
+//  Created by Quentin Perrot on 4/27/16.
+//  Copyright © 2016 Stanford University. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Section: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
